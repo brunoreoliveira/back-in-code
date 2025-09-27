5efd031f8fcf442834571dc68f6e9e4b7778f908
Bruno Oliveira <bruno.oliveira@nexatlas.com>
2025-09-27 13:33:47 -0300

    test(blog): image test on md file test

--- app/pages/posts/[slug].vue
+++ app/pages/posts/[slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="container" class="markdown-body" v-html="html"></div>
+  <div ref="container" class="markdown-body w-full" v-html="html"></div>
 </template>
 
 <script setup lang="ts">
@@ -8,9 +8,9 @@ import mermaid from "mermaid";
 import { v7 as uuid } from "uuid";
 
 const route = useRoute();
-const {data} = await useFetch(`/api/posts/${route.params.slug}`)
-const html = computed(() => data.value?.html ?? '')
-const meta = computed(() => data.value?.meta ?? null)
+const { data } = await useFetch(`/api/posts/${route.params.slug}`);
+const html = computed(() => data.value?.html ?? "");
+const meta = computed(() => data.value?.meta ?? null);
 const container = ref<HTMLDivElement | null>(null);
 
 mermaid.initialize({ startOnLoad: false, theme: "default" });
@@ -31,17 +31,46 @@ async function renderMermaids() {
     if (!code) continue;
 
     const id = `mmd-${uuid()}`;
-    const {svg} = await mermaid.render(id, code);
-    (element.closest('pre') ?? element).replaceWith(htmlToElement(svg))
+    const { svg } = await mermaid.render(id, code);
+    (element.closest("pre") ?? element).replaceWith(htmlToElement(svg));
   }
 }
 
 function htmlToElement(svg: string) {
-  const doc = document.createElement('div');
+  const doc = document.createElement("div");
   doc.innerHTML = svg;
-  return doc.firstElementChild!
+  return doc.firstElementChild!;
 }
 
-onMounted(renderMermaids)
-watch(() => html.value, renderMermaids)
+onMounted(renderMermaids);
+watch(() => html.value, renderMermaids);
 </script>
+
+<style scoped>
+@reference "tailwindcss";
+
+  .markdown-body :deep(h1) {
+    @apply text-6xl font-bold mb-6 flex justify-center items-center p-4;
+  }
+  .markdown-body :deep(h2) {
+    @apply text-2xl font-semibold mt-8 mb-3;
+  }
+  .markdown-body :deep(p) {
+    @apply my-4 leading-7;
+  }
+  .markdown-body :deep(ul) {
+    @apply list-disc ms-6 my-4;
+  }
+  .markdown-body :deep(ol) {
+    @apply list-decimal ms-6 my-4;
+  }
+  .markdown-body :deep(a) {
+    @apply text-blue-600 underline underline-offset-4;
+  }
+  .markdown-body :deep(pre) {
+    @apply w-full my-4 p-6 rounded overflow-x-auto;
+  }
+  .markdown-body :deep(code) {
+    @apply px-1 py-0.5 rounded;
+  }
+</style>
--- content/posts/hello-world.md
+++ content/posts/hello-world.md
@@ -8,7 +8,13 @@ summary: "Post de teste renderizado via markdown-it."
 
 # Título H1
 ```ts
-
+// aqui é o exemplo de um console.log
 console.log("Hello World")
 
+//aqui temos uma declaração de função
+function Exemplo() {
+  return "Hello World";
+}
 ```
+
+![logo](https://cdn.discordapp.com/attachments/1421523732320747534/1421523794622943242/image.png?ex=68d958b3&is=68d80733&hm=69a6f7301273452decbe6a8b00e3454001afe1e94c1a2333fc239309a6b8497a)
